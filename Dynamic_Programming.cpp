039629105d5f6f0b5003e5974b60902cdf697084
kim-dongho <61777992+kim-dongho@users.noreply.github.com>
2020-06-18 22:25:58 +0900

    Dynamic Programming

--- Dynamic_Programming.cpp
+++ Dynamic_Programming.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <cstring>
 #include <algorithm>
-#define MAX 15
+#include <cstring>
+#include <cstdlib>
+#define MAX 20
+
 using namespace std;
 
 int P[5] = {4,6,8,9,6};
 int W[5] = {2,4,5,8,3};
 int n = 5, m = 13;
+int dynamic_f(int a,int b);
+int K[MAX][MAX];
 
 int dynamic_first()
 {
-	int K[MAX][MAX];
-	memset(K,-1,sizeof(int));
 	int i,w;
 	
 	for(w = 0; w <= m; w++)
@@ -40,24 +42,16 @@ int dynamic_first()
 
 int dynamic_second()
 {
-	int K[MAX][MAX];
-	memset(K,-1,sizeof(K));
+	memset(K,0,sizeof(K));
 	int i,w;
 	
 	for(w = 0; w <= m; w++)
 		K[0][w] = 0;
 	for(i = 0; i <= n; i++)
 		K[i][0] = 0;
-	for(i = 1; i <= n; i++)
-	{
-		for(w = 1; w <= m; w++)
-		{
-			if(w < W[i-1] && K[i-1][w] >= 0)
-				K[i][w] = K[i-1][w];
-			
-		}
-	}
 	
+	dynamic_f(n,m);
+
 	for (int i = 0; i <= n; i++) {
 		for (int j = 0; j <= m; j++)
 			cout << K[i][j] << ' ';
@@ -67,17 +61,32 @@ int dynamic_second()
 	return K[n][m];
 }
 
+int dynamic_f(int i, int w)
+{
+	if(i == 1)
+	{
+		if(W[i] <= w){
+			K[i][w] = P[0];
+			return P[i];
+		}
+		else if(0 <= w)
+			return 0;
+	}
+	
+	if(w < 0)
+		return -P[i+1];
+
+	return K[i][w] = max(dynamic_f(i-1, w), dynamic_f(i-1,w-W[i-1])+P[i-1]);
+}
 int main()
 {
 	cout << "동적 계획법 첫 번째 " << endl << endl;
-	int x = dynamic_first();
 	cout << "배낭 크기 = " << m << endl;
-	cout << "최대 이익 = " << x << endl << endl;
+	cout << "최대 이익 = " << dynamic_first() << endl << endl;
 	
 	cout << "동적 계획법 두 번째 " << endl << endl;
-	x = dynamic_second();
 	cout << "배낭 크기 = " << m << endl;
-	cout << "최대 이익 = " << x << endl << endl;
+	cout << "최대 이익 = " << dynamic_second() << endl << endl;
 	
 	/* cout << "동적 계획법 세 번째 " << endl;
 	x = dynamic_third();
